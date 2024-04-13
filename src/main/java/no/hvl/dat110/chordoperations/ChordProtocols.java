807bcaf08ec70df90c0b7173da11ad4bd1f5f697
h602110 <89131024+h602110@users.noreply.github.com>
2024-04-13 11:00:24 +0200

    Done

--- src/main/java/no/hvl/dat110/chordoperations/ChordProtocols.java
+++ src/main/java/no/hvl/dat110/chordoperations/ChordProtocols.java
@@ -1,14 +1,16 @@
 /**
- * 
+ *
  */
 package no.hvl.dat110.chordoperations;
 
 import java.math.BigInteger;
 import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
+import java.util.List;
 import java.util.Set;
 import java.util.Timer;
 
+import no.hvl.dat110.util.Hash;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,10 +21,8 @@ import no.hvl.dat110.util.Util;
 
 /**
  * @author tdoy
- *
  */
 public class ChordProtocols {
-	
 	private static final Logger logger = LogManager.getLogger(ChordProtocols.class);
 	/**
 	 * UpdateSuccessor
@@ -30,16 +30,15 @@ public class ChordProtocols {
 	 * FixFingerTable
 	 * CheckPredecessor
 	 */
-	
-	private NodeInterface chordnode;
+	private final NodeInterface chordnode;
 	private StabilizationProtocols stabprotocol;
-	
+
 	public ChordProtocols(NodeInterface chordnode) {
 		this.chordnode = chordnode;
 		joinRing();
 		stabilizationProtocols();
 	}
-	
+
 	/**
 	 * Public access bcos it's used by the GUI
 	 */
@@ -48,137 +47,143 @@ public class ChordProtocols {
 		stabprotocol = new StabilizationProtocols(this, timer);
 		timer.scheduleAtFixedRate(stabprotocol, 5000, 2000);
 	}
-	
+
 	/**
 	 * Public access bcos it's used by the GUI
 	 */
 	public void joinRing() {
-		
 		//Registry registry = Util.tryIPs();											// try the trackers IP addresses
 		try {
 			Registry registry = Util.tryIPSingleMachine(chordnode.getNodeName());
- 			// try the ports
-			if(registry != null) {
+			// try the ports
+			if (registry != null) {
 				try {
 					String foundNode = Util.activeIP;
-	
+
 					NodeInterface randomNode = (NodeInterface) registry.lookup(foundNode);
-					
-					logger.info("JoinRing-randomNode = "+randomNode.getNodeName());
-					
+
+					logger.info("JoinRing-randomNode = " + randomNode.getNodeName());
+
 					// call remote findSuccessor function. The result will be the successor of this randomNode
 					NodeInterface chordnodeSuccessor = randomNode.findSuccessor(chordnode.getNodeID());
-	
+
 					// set the successor of this node to chordnodeSuccessor and its predecessor to null
-					chordnode.setSuccessor(chordnodeSuccessor);	
+					chordnode.setSuccessor(chordnodeSuccessor);
 					chordnode.setPredecessor(null);
-					
+
 					// notify chordnodeSuccessor of a new predecessor
 					chordnodeSuccessor.notify(chordnode);
-					
+
 					// fix the finger table - create a new routing table for this node
 					fixFingerTable();
-					
+
 					// copy all keys that are less or equal (<=) to chordnode ID to chordnode
 					((Node) chordnode).copyKeysFromSuccessor(chordnodeSuccessor);
-					
-					logger.info(chordnode.getNodeName()+" is between null | "+chordnodeSuccessor.getNodeName());
-					
+
+					logger.info(chordnode.getNodeName() + " is between null | " + chordnodeSuccessor.getNodeName());
 				} catch (Exception e) {
 					logger.error(e.getMessage());
 				}
 			} else {
-				
-				createRing((Node) chordnode);		// no node is available, create a new ring
+				createRing((Node) chordnode);        // no node is available, create a new ring
 			}
-		} catch (NumberFormatException | RemoteException e1) {
-			// TODO Auto-generated catch block
+		} catch (NumberFormatException | RemoteException ignored) {
 		}
 	}
-	
+
 	private void createRing(Node node) throws RemoteException {
-		
 		// set predecessor to nil - No predecessor for now
 		node.setPredecessor(null);
-		
+
 		// set the successor to itself
 		node.setSuccessor(node);
-		
-		logger.info("New ring created. Node = "+node.getNodeName()+" | Successor = "+node.getSuccessor().getNodeName()+
-				" | Predecessor = "+node.getPredecessor());
-		
+
+		logger.info("New ring created. Node = " + node.getNodeName() + " | Successor = " + node.getSuccessor().getNodeName() + " | Predecessor = " + node.getPredecessor());
 	}
-	
+
 	public void leaveRing() throws RemoteException {
-		
 		logger.info("Attempting to update successor and predecessor before leaving the ring...");
-		
+
 		try {
-		 
-			NodeInterface prednode = chordnode.getPredecessor();														// get the predecessor			
-			NodeInterface succnode = chordnode.getSuccessor();														// get the successor		
-			
-			Set<BigInteger> keyids = chordnode.getNodeKeys();									// get the keys for chordnode
-			
-			if(succnode != null) {												// add chordnode's keys to its successor
+			NodeInterface prednode = chordnode.getPredecessor();                                                        // get the predecessor
+			NodeInterface succnode = chordnode.getSuccessor();                                                        // get the successor
+
+			Set<BigInteger> keyids = chordnode.getNodeKeys();                                    // get the keys for chordnode
+
+			if (succnode != null) {                                                // add chordnode's keys to its successor
 				keyids.forEach(fileID -> {
 					try {
-						logger.info("Adding fileID = "+fileID+" to "+succnode.getNodeName());
+						logger.info("Adding fileID = " + fileID + " to " + succnode.getNodeName());
 
 						succnode.addKey(fileID);
-						Message msg = chordnode.getFilesMetadata().get(fileID);				
-						succnode.saveFileContent(msg.getNameOfFile(), fileID, msg.getBytesOfFile(), msg.isPrimaryServer()); 			// save the file in memory of the newly joined node
-					} catch (RemoteException e) {
-						//
-					} 
+						Message msg = chordnode.getFilesMetadata().get(fileID);
+						succnode.saveFileContent(msg.getNameOfFile(), fileID, msg.getBytesOfFile(), msg.isPrimaryServer());            // save the file in memory of the newly joined node
+					} catch (RemoteException ignored) {
+					}
 				});
 
-				succnode.setPredecessor(prednode); 							// set prednode as the predecessor of succnode
+				succnode.setPredecessor(prednode);                            // set prednode as the predecessor of succnode
 			}
-			if(prednode != null) {
-				prednode.setSuccessor(succnode);							// set succnode as the successor of prednode			
-			} 
+			if (prednode != null) {
+				prednode.setSuccessor(succnode);                            // set succnode as the successor of prednode
+			}
 			chordnode.setSuccessor(chordnode);
 			chordnode.setPredecessor(chordnode);
 			chordnode.getNodeKeys().clear();
 			stabprotocol.setStop(true);
-			
-		}catch(Exception e) {
-			//
-			logger.error("some errors while updating succ/pred/keys...\n"+e.getMessage());
+
+		} catch (Exception e) {
+			logger.error("some errors while updating succ/pred/keys...\n" + e.getMessage());
 		}
-		
+
 		logger.info("Update of successor and predecessor completed...bye!");
 	}
-	
+
 	public void fixFingerTable() {
-		
 		try {
-			logger.info("Fixing the FingerTable for the Node: "+ chordnode.getNodeName());
-	
+			logger.info("Fixing the FingerTable for the Node: " + chordnode.getNodeName());
+
 			// get the finger table from the chordnode (list object)
-			
 			// ensure to clear the current finger table
-			
 			// get the address size from the Hash class. This is the modulus and our address space (2^mbit = modulus)
-			
 			// get the number of bits from the Hash class. Number of bits = size of the finger table
-			
-			// iterate over the number of bits			
-			
+			// iterate over the number of bits
 			// compute: k = succ(n + 2^(i)) mod 2^mbit
-			
 			// then: use chordnode to find the successor of k. (i.e., succnode = chordnode.findSuccessor(k))
-			
 			// check that succnode is not null, then add it to the finger table
 
+			int s = Hash.bitSize();
+			List<NodeInterface> fingerTable = ((Node) chordnode).getFingerTable();
+			BigInteger mod = Hash.addressSize();
+
+			NodeInterface succnode = null;
+			for (int i = 0; i < s; i++) {
+				BigInteger nextID = new BigInteger("2");
+				nextID = nextID.pow(i);
+
+				BigInteger currentID = chordnode.getNodeID().add(nextID);
+				currentID = currentID.mod(mod);
+
+				try {
+					succnode = chordnode.findSuccessor(currentID);
+				} catch (RemoteException e) {
+					e.printStackTrace();
+				}
+
+				if (succnode != null) {
+					try {
+						fingerTable.set(i, succnode);
+					} catch (IndexOutOfBoundsException e) {
+						fingerTable.add(i, succnode);
+					}
+				}
+			}
 		} catch (RemoteException e) {
-			//
+			e.printStackTrace();
 		}
 	}
 
 	protected NodeInterface getChordnode() {
 		return chordnode;
 	}
-
 }
--- src/main/java/no/hvl/dat110/util/Hash.java
+++ src/main/java/no/hvl/dat110/util/Hash.java
@@ -2,65 +2,65 @@ package no.hvl.dat110.util;
 
 /**
  * exercise/demo purpose in dat110
- * @author tdoy
  *
+ * @author tdoy
  */
 
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class Hash { 
-	
-	
-	public static BigInteger hashOf(String entity) {	
-		
+public class Hash {
+	public static BigInteger hashOf(String entity) {
 		BigInteger hashint = null;
-		
+
 		// Task: Hash a given string using MD5 and return the result as a BigInteger.
-		
 		// we use MD5 with 128 bits digest
-		
 		// compute the hash of the input 'entity'
-		
 		// convert the hash into hex format
-		
 		// convert the hex into BigInteger
-		
 		// return the BigInteger
-		
+
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte[] messageDigest = md.digest(entity.getBytes());
+			String hex = toHex(messageDigest);
+			hashint = new BigInteger(hex, 16);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
 		return hashint;
 	}
-	
+
 	public static BigInteger addressSize() {
-		
 		// Task: compute the address size of MD5
-		
 		// compute the number of bits = bitSize()
-		
 		// compute the address size = 2 ^ number of bits
-		
 		// return the address size
-		
-		return null;
-	}
-	
-	public static int bitSize() {
-		
-		int digestlen = 0;
-		
-		// find the digest length
-		
-		return digestlen*8;
-	}
-	
-	public static String toHex(byte[] digest) {
-		StringBuilder strbuilder = new StringBuilder();
-		for(byte b : digest) {
-			strbuilder.append(String.format("%02x", b&0xff));
-		}
-		return strbuilder.toString();
+
+		return BigInteger.valueOf(2).pow(bitSize());
 	}
 
+	public static int bitSize() {
+		int digestlen = 0;
+
+		// find the digest length
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			digestlen = md.getDigestLength();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
+		return digestlen * 8;
+	}
+
+	public static String toHex(byte[] digest) {
+		StringBuilder data = new StringBuilder();
+		for (byte b : digest) {
+			data.append(String.format("%02x", b & 0xff));
+		}
+		return data.toString();
+	}
 }
